2b895435447f40339fcb78e1e3b50adedd2b91d4
abelzyp <abelzyp@foxmail.com>
2016-08-29 22:30:19 +0800

    Beautiful Code

---
+++ src/BeautifulCode/Code1.java
@@ -0,0 +1,14 @@
+package BeautifulCode;
+
+/*
+ * 我能让我美丽的代码，以这个白色的笑脸开始么？
+ */
+public class Code1 {
+	public static void main(String[] args) {
+		System.out.println('\u263A');
+	}
+
+	enum Weekday {
+		MON, TUE, WED, THU, FRI, SAT, SUN
+	};
+}
